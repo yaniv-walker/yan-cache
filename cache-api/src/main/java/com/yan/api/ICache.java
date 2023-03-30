49c30ae92381020b223fdcd163842fbc885cdd02
yaniv-walker <yanjiaqi.it@foxmail.com>
2023-03-30 23:51:16 +0800

    build project structure

---
+++ cache-api/src/main/java/com/yan/api/ICache.java
@@ -0,0 +1,14 @@
+package com.yan.api;
+
+import java.util.Map;
+
+/**
+ * Cache interface.
+ *
+ * @author yanjiaqi
+ * @version 1.0.0
+ * @date 2023/3/30 0030
+ * @since JDK 1.8.0
+ */
+public interface ICache<K, V> extends Map<K, V> {
+}
