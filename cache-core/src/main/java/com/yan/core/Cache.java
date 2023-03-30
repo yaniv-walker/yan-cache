8ddf319725a44e8d5e3cb7a03da692dd121ed698
yaniv-walker <yanjiaqi.it@foxmail.com>
2023-03-30 23:58:04 +0800

    build project structure

---
+++ cache-core/src/main/java/com/yan/core/Cache.java
@@ -0,0 +1,14 @@
+package com.yan.core;
+
+import com.yan.api.ICache;
+
+/**
+ * .
+ *
+ * @author yanjiaqi
+ * @version 1.0.0
+ * @date 2023/3/30 0030
+ * @since JDK 1.8.0
+ */
+public class Cache {
+}
